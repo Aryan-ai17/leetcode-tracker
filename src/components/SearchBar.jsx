f16b9e069c787081a9522fbfb9ee56ae5fe55bb1
Aryan yadav <aryanyadav1217@gmail.com>
2026-06-05 00:44:26 +0530

    Initial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter LeetCode username"
+      />
+
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar.jsx";
+import SearchBar from "../components/SearchBar.jsx";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+
+      <h2>Track Your LeetCode Progress</h2>
+    </div>
+  );
+}
+
+export default Home;
